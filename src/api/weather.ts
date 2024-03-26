59527c2b93d6fe16a9f879429b623e9daf728a3b
Eric Maya <ericmaya8a@gmail.com>
2024-03-26 15:36:55 -0600

    update type

--- src/api/weather.ts
+++ src/api/weather.ts
@@ -1,25 +1,29 @@
 const BASE_URL = "https://api.weatherapi.com/v1";
 
+type ReturnValue<T> = Promise<T | undefined>;
+
 async function get(url: string) {
   const response = await fetch(url);
   return await response.json();
 }
 
-export async function fetchLocations(city: string): Promise<LocationType[]> {
+export async function fetchLocations(
+  city: string
+): ReturnValue<LocationType[]> {
   try {
     return get(
       `${BASE_URL}/search.json?key=${process.env.WEATHER_API_KEY}&q=${city}`
     );
   } catch (error) {
     console.error("Error while fetching locations", error);
-    return [];
+    return undefined;
   }
 }
 
 export async function fetchWeatherForecast(
   city: string,
   days: number = 7
-): Promise<Forecast | undefined> {
+): ReturnValue<Forecast> {
   try {
     return get(
       `${BASE_URL}/forecast.json?key=${process.env.WEATHER_API_KEY}&q=${city}&days=${days}&aqi=no&alerts=no`
